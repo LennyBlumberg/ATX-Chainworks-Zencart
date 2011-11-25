13eba73b198d245a755fb5d08ddc704fda0c2cb9
Lenny Blumberg <lenny@lennyblumberg.com>
2011-11-24 21:20:39 -0600

    Updated template_info.php

--- includes/templates/ATXCustom/template_info.php
+++ includes/templates/ATXCustom/template_info.php
@@ -8,9 +8,9 @@
  * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
  * @version $Id: template_info.php 2306 2005-11-05 21:34:28Z wilt $
  */
-$template_name = 'Default Template';
+$template_name = 'ATX Chainworks Custom Template';
 $template_version = 'Version 1.0';
-$template_author = 'Zen Cart Team (c) 2003';
-$template_description = 'This template set is designed to be easily modified using only the style sheet to change colors, fonts, and the store logo. Three images are required; logo.jpg, header_bg.jpg, and tile_back.jpg.';
+$template_author = 'Lendon Computer Services LLC (c) 2011';
+$template_description = 'Custom ZenCart template developed for the ATX Chainworks online store.';
 $template_screenshot = 'scr_template_default.jpg';
 ?>
