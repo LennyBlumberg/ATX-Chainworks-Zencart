6675092a84f382fd31b773acc3ed7acf96cc021a
Lenny Blumberg <lenny@lennyblumberg.com>
2011-11-24 21:43:01 -0600

    Updated version number

--- includes/templates/ATXCustom/template_info.php
+++ includes/templates/ATXCustom/template_info.php
@@ -9,7 +9,7 @@
  * @version $Id: template_info.php 2306 2005-11-05 21:34:28Z wilt $
  */
 $template_name = 'ATX Chainworks Custom Template';
-$template_version = 'Version 1.0';
+$template_version = 'Version 1.1';
 $template_author = 'Lendon Computer Services LLC (c) 2011';
 $template_description = 'Custom ZenCart template developed for the ATX Chainworks online store.';
 $template_screenshot = 'scr_template_default.jpg';
